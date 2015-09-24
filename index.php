291f53e8c54ed064339bcd1c21f34ec036db1e2e
Alejandro Henkel <alehenkel17@gmail.com>
2015-09-24 17:54:34 -0500

    Add conocenos

--- index.php
+++ index.php
@@ -11,20 +11,22 @@
 	<link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/buttons.css">
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="js/jquery-1.11.3.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	<script src="js/parallax.min.js"></script>
+	<script src="js/custom.js"></script>
 </head>
-<body>
-	<section class="iu-intro">
-		<div class="intro parallax-window visible-xs-block" data-parallax="scroll" data-image-src="images/bg-1.jpg"></div>
+<body data-spy="scroll" data-target="#bs-example-navbar-collapse-1">
+	<section class="iu-section iu-intro">
+		<div class="intro parallax-intro visible-xs-block" data-parallax="scroll" data-image-src="images/bg-1.jpg"></div>
 		<video autoplay loop poster="images/bg-1.jpg" class="bg-video hidden-xs">
 			<source src="images/videos/timelapse.webm" type="video/webm">
 			<source src="images/videos/timelapse.mp4" type="video/mp4">
 		</video>
 		<div class="overlay">
-			<nav class="navbar navbar-fixed-top">
+			<nav class="navbar navbar-fixed-top" data-spy="affix" data-offset-top="250" data-offset-bottom="500">
 			  <div class="container-fluid">
 			    <div class="border-b-white">
 			    	<!-- Brand and toggle get grouped for better mobile display -->
@@ -44,7 +46,7 @@
 				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				      
 				      <ul class="nav navbar-nav navbar-right">
-				        <li><a href="#">Conócenos</a></li>
+				        <li><a href="#iu-conocenos">Conócenos</a></li>
 				        <li class="dropdown">
 				          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Proyectos <span class="caret"></span></a>
 				          <ul class="dropdown-menu">
@@ -65,13 +67,37 @@
 			  </div><!-- /.container-fluid -->
 			</nav>
 			<div class="container intro-height" align="center">
-				<div class="wrapper-img">
+				<div class="vertical-align">
 					<img class="intro-logos" src="images/dos-logos.png" alt="">
 				</div>
 			</div>
+		</div>	
+	</section>
+
+	<section class="iu-section" id="iu-conocenos">
+		<div class="container-fluid">
+			<div class="row">
+				<div>
+					<div class="col-xs-12 col-sm-6 h-400 padding-small">
+						<div class="vertical-align-no-small">
+							<p class="main-text">
+								Somos un <b>PROGRAMA</b> de Servicio Social e investigación que explora <b>OPORTUNIDADES</b> de <b>MEJORAMIENTO DE VIVIENDA</b> y <b>ESPACIOS PÚBLICOS</b> en zonas de alta marginación y riesgo para la salud con una <b>VISIÓN INTEGRAL</b>
+							</p>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-6 bg-red h-400 padding-small">
+						<div class="vertical-align-no-small" align="center">
+							<button class="button button--inverted button--wayra button--border-thick button--text-upper button--size-s uppercase">Conoce más</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-		
-		
+		<div class="container-parallax" data-parallax="scroll" data-image-src="images/bg-4.jpg">
+			<div class="container container-conocenos vertical-align col-white">
+				<h1>Desarrollando Comunidades Integrales Por Una Vida Sana, Productiva y Significativa</h1>
+			</div>
+		</div>
 	</section>
 </body>
 </html>
